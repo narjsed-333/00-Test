e0e41c0c7aef521fcc20f535c007fabebd6f112c
nseddiki <nseddiki@350028R-F115X03.lise.kreizenn.bzh>
2026-09-22 12:40:28 +0200

    20260921_SEDDIKI_Narjess-TP01_JAVA

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
-
+import java.util.Scanner;
 /**
  *
  * @author nseddiki
@@ -14,8 +14,18 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world ggg");
+        
+        int x, y;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello world");
         // TODO code application logic here
+    
+      
+        System.out.print("Inserer une valeur x ");
+        x = sc.nextInt();
+      
+             System.out.print("Inserer une valeur y ");
+        y = sc.nextInt();      
     }
     
 }
