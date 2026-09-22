66e25ce7075c8ee2a1cf60b3e65b13db597fde54
nseddiki <nseddiki@350028R-F115X03.lise.kreizenn.bzh>
2026-09-22 11:36:28 +0200

    nouveau commit 2

--- src/pkg00/helloworld/HelloWorld.java
+++ src/pkg00/helloworld/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello world ggg");
         // TODO code application logic here
     }
     
